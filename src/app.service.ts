b78e6f574a3195d1d227e26cf264013c3eed5e5e
Sahil Kumar <sahilkumar142002@gmail.com>
2026-03-25 17:42:21 +0530

    feat: update greeting message in AppService to provide a more humorous response

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Get out of here, this is not the API you are looking for!';
   }
 }
